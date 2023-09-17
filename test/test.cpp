0c9c565ac864b0a482252bd569724dbbc82a3fef
诺诺 <117335537+AnceYin@users.noreply.github.com>
2023-09-17 17:00:41 +0800

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
 #include "pipe.hpp"
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <vector>
+#include <limits>
 
-int main() {
-	"test\n" | [](auto i){std::cout<<i;};
+
+int main()
+{
+    std::numeric_limits<double>::lowest()
+    auto start = std::chrono::system_clock::now();
+    "test\n" | [](auto i){std::cout<<i;};
+    auto end = std::chrono::system_clock::now();
+
+    std::random_device rd;  // 种子 
+    std::mt19937 gen(rd()); // 引擎
+    std::uniform_int_distribution<> dis_i(1, 6);//[1,6]
+    std::uniform_real_distribution<> dis_d(1, 2);//[1, 2)
+    dis_i(gen);//调用
+ 
+    std::chrono::duration<double> diff = end-start;
 }
